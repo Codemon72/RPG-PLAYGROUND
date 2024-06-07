fa87be38fcd739e9d762ba2660561b2fc7b76ebc
Clemens Bruesch <clemensbruesch@gmail.com>
2024-06-07 23:36:34 +0200

    feat: generate attribute levels with random values from 0 to 3; summing up to 4

--- BoLfiles/BoL_app.js
+++ BoLfiles/BoL_app.js
@@ -1,18 +1,16 @@
 // DOCUMENT REFERENCE VARIABLES //
 const possibleChoices = document.querySelectorAll('button')
-const careerDiv = document.getElementById('career')
+const attributesDiv = document.getElementById('attributes')
+const careersDiv = document.getElementById('careers')
 
 // ALL TABLES //
+const attributesArray = ['Strength', 'Agility', 'Mind', 'Appeal']
+
 const careersArray = [
   'Alchemist',
   'Assassin',
   'Barbarian',
-  'Beastmaster',
-  'Beggar',
   'Blacksmith',
-  'Dancer',
-  'Executioner',
-  'Farmer',
   'Gladiator',
   'Hunter',
   'Magician',
@@ -21,15 +19,15 @@ const careersArray = [
   'Minstrel',
   'Noble',
   'Physician',
-  'Priest',
-  'Sailor',
+  'Pilot',
+  'Pirate',
+  'Priest/Druid',
+  'Serving Wench',
   'Scribe',
-  'Sky-Pilot',
   'Slave',
   'Soldier',
-  'Temptress',
   'Thief',
-  'Worker',
+  'Torturer',
 ]
 
 // BASIC FUNCTIONS //
@@ -44,12 +42,35 @@ function randomValue(max) {
 }
 
 // SPECIFIC FUNCTIONS //
-function getCareer() {
-  careerDiv.innerHTML = rollCareer()
+function getAttributes() {
+  attributesDiv.innerHTML = rollAttributes()
+}
+function getCareers() {
+  careersDiv.innerHTML = rollCareers()
 }
 
 // CHARACTER BUILDING BLOCKS //
-function rollCareer() {
+function rollAttributes() {
+  let pcAttributes = attributesArray.map((attr) => ({
+    attribute: attr,
+    level: 0,
+  }))
+  let currentTotal = 0
+
+  while (currentTotal < 4) {
+    let tempAttribute = randomMath(pcAttributes)
+    if (tempAttribute.level < 3) {
+      tempAttribute.level += 1
+      currentTotal += 1
+    }
+  }
+
+  return pcAttributes
+    .map((attribute) => `${attribute.attribute} (${attribute.level})`)
+    .join(', ')
+}
+
+function rollCareers() {
   pcCareers = []
   maxTotalRanks = 4
   while (pcCareers.length < 4) {
@@ -71,15 +92,20 @@ function rollCareer() {
       maxTotalRanks = maxTotalRanks - tempValue
     }
   }
+  console.log(pcCareers)
   return pcCareers.map((career) => `${career.name} (${career.rank})`).join(', ')
 }
+// [{attribute: 'Strength', level: 0},{attribute:  'Agility', level: 0}, {attribute: 'Mind', level: 0}, {attribute: 'Appeal', level: 0}]
 
 // BUTTON FUNCTIONS //
 possibleChoices.forEach((possibleChoice) =>
   possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
+    if (userChoice === 'attributes-btn') {
+      getAttributes()
+    }
     if (userChoice === 'career-btn') {
-      getCareer()
+      getCareers()
     }
     console.log('User pressed btn: ' + userChoice)
   })
